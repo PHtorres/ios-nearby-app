25e60d84f537a6306d2729b91d6a5b0e0d1fa787
Paulo Henrique Torres <paulohenriquetorresdev@gmail.com>
2024-12-14 17:56:24 -0300

    add home table cell

---
+++ Nearby/Sources/Features/Models/PlaceModel.swift
@@ -0,0 +1,14 @@
+//
+//  PlaceModel.swift
+//  Nearby
+//
+//  Created by Paulo Henrique Torres on 14/12/24.
+//
+
+import Foundation
+
+struct Place{
+    let imageName:String
+    let title: String
+    let description: String
+}
---
+++ Nearby/Sources/Features/Scenes/Home/HomeViewController.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewController.swift
+//  Nearby
+//
+//  Created by Paulo Henrique Torres on 14/12/24.
+//
+
+import Foundation
